6eb7ffe10b4e71eb557800911240759226cf2521
Tom Augspurger <taugspurger@anaconda.com>
2019-02-11 11:33:00 -0800

    no-cuda

---
+++ recipes/ucx/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+
+# Questions:
+# 1. --disable-numa required. Check on how to enable it
+# 2. --with-cuda is wrong in general.
+# 3. --with-gdrcopy is probably wrong
+
+./configure --prefix="$PREFIX" \
+    --disable-cma \
+    --disable-numa \
+    --disable-cuda \
+    --enable-mt
+
+make install
